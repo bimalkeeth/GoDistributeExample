85d98ada90c34f32cbf0fe62dd68da1ce97b13b3
Bimalkeeth <bkaluarachchi@assetic.com>
2018-12-07 09:24:23 +1100

    first Example

--- Examp01_Gorouting.go
+++ Examp01_Gorouting.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	port := 8080
+	http.HandleFunc("/helloworld", helloWorldHandler)
+	log.Printf("Server is starting on %v\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(writer http.ResponseWriter, request *http.Request) {
+
+	fmt.Fprintf(writer, "Hello world")
 }
